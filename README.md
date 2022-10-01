b040efddb01fc280bc5a87fa0990a74d550e5a19
Hansaka Anuhas <46843880+Hansaka-Anuhas-1@users.noreply.github.com>
2022-10-01 15:54:01 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Manual-Filter-Bot
+### Deploy to Heroku
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/SLHansakaAnuhas/SLFiltersBot)
---
+++ script.py
@@ -0,0 +1,20 @@
+class Script(object):
+
+    START_MSG = """Hello {} 👋
+I'm An Advanced Filters Bot 😎
+Add Me As Admin In Your Groups 🤩"""
+
+    HELP_MSG = """Here Is The Help For My Commands
+
+<b>Filters Commands:</b>
+/filter - Add Filter
+/del - Delete Filter
+/delall - Delete All filters
+/filters - List All Filters
+
+<b>Connections Commands:</b>
+/connect - Connect PM
+/connections - List All Connections"""
+
+    ABOUT_MSG = """My Name: SL Filters Bot
+Creator: <a href=https://t.me/Hansaka_Anuhas>Hansaka Anuhas</a> 🇱🇰"""
